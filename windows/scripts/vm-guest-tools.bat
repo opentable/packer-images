47e371440d3daed1dc057895e2938a059a733403
liamjbennett <lbennett@opentable.com>
2014-09-15 14:43:29 +0100

    adding support for windows clients and tweaking vm-guest-tools

--- windows/scripts/vm-guest-tools.bat
+++ windows/scripts/vm-guest-tools.bat
@@ -1,16 +1,10 @@
-reg.exe query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PROCESSOR_ARCHITECTURE | find /i "AMD64"
-set BITNESS=%errorlevel%
+set VIDEO=
+for /f "tokens=2 delims==" %%a in ('wmic path win32_videocontroller Where DeviceID="VideoController1" get Description /value^|find "="') do @set VIDEO=%%a
 
-if %BITNESS% EQU 0 (
-    if not exist "C:\Windows\Temp\7z920-x64.msi" (
-      a:\downloadFile.vbs "http://heanet.dl.sourceforge.net/project/sevenzip/7-Zip/9.20/7z920-x64.msi" "C:\Windows\Temp\7z920-x64.msi"
-    )
-    msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
-) else (
-    if not exist "C:\Windows\Temp\7z920.msi" (
-      a:\downloadFile.vbs http://heanet.dl.sourceforge.net/project/sevenzip/7-Zip/9.20/7z920.msi" "C:\Windows\Temp\7z920.msi"
-    )
-    msiexec /qb /i C:\Windows\Temp\7z920.msi
+Echo.%VIDEO% | find /i "VMware">Nul && (
+  set PACKER_BUILDER_TYPE=vmware-iso
+) || (
+  set PACKER_BUILDER_TYPE=virtualbox-iso
 )
 
 if "%PACKER_BUILDER_TYPE%" equ "vmware-iso" goto :vmware
@@ -19,41 +13,18 @@ goto :done
 
 :vmware
 
-if exist "C:\Users\vagrant\windows.iso" (
-    move /Y C:\Users\vagrant\windows.iso C:\Windows\Temp
-)
-
-if not exist "C:\Windows\Temp\windows.iso" (
-    C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('http://softwareupdate.vmware.com/cds/vmw-desktop/ws/10.0.1/1379776/windows/packages/tools-windows-9.6.1.exe.tar', 'C:\Windows\Temp\vmware-tools.exe.tar')" <NUL
-    cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\vmware-tools.exe.tar -oC:\Windows\Temp"
-    FOR /r "C:\Windows\Temp" %%a in (tools-windows-*.exe) DO REN "%%~a" "tools-windows.exe"
-    cmd /c C:\Windows\Temp\tools-windows
-    move /Y "C:\Program Files (x86)\VMware\tools-windows\windows.iso" C:\Windows\Temp
-    rd /S /Q "C:\Program Files (x86)\VMWare"
-)
-
-cmd /c ""C:\Program Files\7-Zip\7z.exe" x -y "C:\Windows\Temp\windows.iso" -oC:\Windows\Temp\VMWare"
-cmd /c C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
+cmd /c E:\setup.exe /S /v"/qn REBOOT=R\"
 
 goto :done
 
 :virtualbox
+echo "Virtualbox install" >> C:\Windows\Temp\tools-install.txt
 
 :: There needs to be Oracle CA (Certificate Authority) certificates installed in order
 :: to prevent user intervention popups which will undermine a silent installation.
 cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
 
-move /Y "C:\Users\vagrant\VBoxGuestAdditions.iso" C:\Windows\Temp
-
-cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
-cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
-timeout 10 > null
+cmd /c E:\VBoxWindowsAdditions.exe /S
 goto :done
 
 :done
-
-if %BITNESS% EQU 0 (
-    msiexec /qb /x C:\Windows\Temp\7z920-x64.msi
-) else (
-    msiexec /qb /x C:\Windows\Temp\7z920.msi
-)
