207e1bfdbb830eb4ba83a33bff06e5265a8bc98e
liamjbennett <lbennett@opentable.com>
2014-09-16 20:26:21 +0100

    fixing the silent failures in winrm script

--- windows/scripts/winrm.bat
+++ windows/scripts/winrm.bat
@@ -1,13 +1,11 @@
-winrm quickconfig -q
-winrm quickconfig -transport:http
-winrm quickconfig -transport:http
-winrm set winrm/config @{MaxTimeoutms="1800000"}
-winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/auth @{Basic="true"}
-winrm set winrm/config/client/auth @{Basic="true"}
-winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
-netsh firewall add portopening TCP 5985 "Port 5985"
-net stop winrm
-sc config winrm start= auto
-net start winrm
+cmd /c winrm quickconfig -q
+cmd /c winrm quickconfig -transport:http
+cmd /c winrm set winrm/config @{MaxTimeoutms="1800000"}
+cmd /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+cmd /c winrm set winrm/config/service @{AllowUnencrypted="true"}
+cmd /c winrm set winrm/config/service/auth @{Basic="true"}
+cmd /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+cmd /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
+cmd /c netsh firewall add portopening TCP 5985 "Port 5985"
+cmd /c net stop winrm
+cmd /c net start winrm
