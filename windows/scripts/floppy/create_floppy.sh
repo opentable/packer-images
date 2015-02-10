9c67b677c5c5a93c9512cb7e886ee4698b01a528
liamjbennett <lbennett@opentable.com>
2015-02-10 17:01:48 +0000

    building windows 2003 and 2003R2 images

--- windows/scripts/floppy/create_floppy.sh
+++ windows/scripts/floppy/create_floppy.sh
@@ -6,28 +6,34 @@ fi
 
 vagrant up
 
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../../templates/windows-2003R2-serverenterprise-amd64/winnt.sif vagrant@localhost:/home/vagrant/winnt.sif
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../regedit.bat vagrant@localhost:/home/vagrant/regedit.bat
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../runonce.bat vagrant@localhost:/home/vagrant/runonce.bat
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../install-cygwin-sshd.bat vagrant@localhost:/home/vagrant/install-cygwin-sshd.bat
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../ps.bat vagrant@localhost:/home/vagrant/ps.bat
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../downloadFile.vbs vagrant@localhost:/home/vagrant/downloadFile.vbs
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../vagrant-ssh.bat vagrant@localhost:/home/vagrant/vagrant-ssh.bat
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../oracle-cert.cer vagrant@localhost:/home/vagrant/oracle-cert.cer
-scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../winrm.bat vagrant@localhost:/home/vagrant/winrm.bat
-
 vagrant ssh -c "sudo dd bs=512 count=2880 if=/dev/zero of=floppy01.img"
 vagrant ssh -c "sudo mkfs.msdos floppy01.img"
 vagrant ssh -c "sudo mount -o loop floppy01.img /mnt"
-vagrant ssh -c "sudo cp /home/vagrant/winnt.sif /mnt/winnt.sif"
-vagrant ssh -c "sudo cp /home/vagrant/regedit.bat /mnt/regedit.bat"
-vagrant ssh -c "sudo cp /home/vagrant/runonce.bat /mnt/runonce.bat"
-vagrant ssh -c "sudo cp /home/vagrant/install-cygwin-sshd.bat /mnt/install-cygwin-sshd.bat"
-vagrant ssh -c "sudo cp /home/vagrant/vagrant-ssh.bat /mnt/vagrant-ssh.bat"
-vagrant ssh -c "sudo cp /home/vagrant/ps.bat /mnt/ps.bat"
-vagrant ssh -c "sudo cp /home/vagrant/downloadFile.vbs /mnt/downloadFile.vbs"
-vagrant ssh -c "sudo cp /home/vagrant/oracle-cert.cer /mnt/oracle-cert.cer"
-vagrant ssh -c "sudo cp /home/vagrant/winrm.bat /mnt/winrm.bat"
+vagrant ssh -c "sudo mkdir /tmp/floppy && sudo chown vagrant:vagrant /tmp/floppy"
+
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../../templates/$1/winnt.sif vagrant@localhost:/tmp/floppy/winnt.sif
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../regedit.bat vagrant@localhost:/tmp/floppy/regedit.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../runonce.bat vagrant@localhost:/tmp/floppy/runonce.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../bitvisessh.bat vagrant@localhost:/tmp/floppy/bitvisessh.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../BvSshServer-Settings.wst vagrant@localhost:/tmp/floppy/BvSshServer-Settings.wst
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../ps.bat vagrant@localhost:/tmp/floppy/ps.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../downloadFile.vbs vagrant@localhost:/tmp/floppy/downloadFile.vbs
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../vagrant-ssh.bat vagrant@localhost:/tmp/floppy/vagrant-ssh.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../oracle-cert.cer vagrant@localhost:/tmp/floppy/oracle-cert.cer
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../winrm.bat vagrant@localhost:/tmp/floppy/winrm.bat
+scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i ~/.vagrant.d/insecure_private_key -P 2222 ../vm-guest-tools.bat vagrant@localhost:/tmp/floppy/vm-guest-tools.bat
+
+vagrant ssh -c "sudo cp /tmp/floppy/winnt.sif /mnt/winnt.sif"
+vagrant ssh -c "sudo cp /tmp/floppy/regedit.bat /mnt/regedit.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/runonce.bat /mnt/runonce.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/bitvisessh.bat /mnt/bitvisessh.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/BvSshServer-Settings.wst /mnt/BvSshServer-Settings.wst"
+vagrant ssh -c "sudo cp /tmp/floppy/vagrant-ssh.bat /mnt/vagrant-ssh.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/ps.bat /mnt/ps.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/downloadFile.vbs /mnt/downloadFile.vbs"
+vagrant ssh -c "sudo cp /tmp/floppy/oracle-cert.cer /mnt/oracle-cert.cer"
+vagrant ssh -c "sudo cp /tmp/floppy/winrm.bat /mnt/winrm.bat"
+vagrant ssh -c "sudo cp /tmp/floppy/vm-guest-tools.bat /mnt/vm-guest-tools.bat"
 vagrant ssh -c "sudo chown root:root /mnt/*.*"
 vagrant ssh -c "sudo umount /mnt"
 
--- windows/scripts/ps.bat
+++ windows/scripts/ps.bat
@@ -1,9 +1,9 @@
 
 IF "%1%"=="win2008_64" (
-   timeout 20 > null   
+   timeout 20 > null
    IF NOT EXIST "C:\Windows6.0-KB968930-x64.msu" (
      ECHO "Downloading Powershell 2.0"
-     C:\Windows\System32\cscript.exe a:\downloadFile.vbs "http://download.microsoft.com/download/2/8/6/28686477-3242-4E96-9009-30B16BED89AF/Windows6.0-KB968930-x64.msu" "C:\Windows6.0-KB968930-x64.msu" 
+     C:\Windows\System32\cscript.exe a:\downloadFile.vbs "http://download.microsoft.com/download/2/8/6/28686477-3242-4E96-9009-30B16BED89AF/Windows6.0-KB968930-x64.msu" "C:\Windows6.0-KB968930-x64.msu"
      timeout 10 > null
      ECHO "Installing Powershell 2.0"
      C:\Windows\System32\wusa.exe C:\Windows6.0-KB968930-x64.msu /quiet /norestart
@@ -29,7 +29,7 @@ IF "%1%"=="win2008_64" (
    )
    IF NOT EXIST "C:\Windows6.0-KB968930-x64.msu" (
      ECHO "Downloading Powershell 2.0"
-     C:\Windows\System32\cscript.exe a:\downloadFile.vbs "http://download.microsoft.com/download/2/8/6/28686477-3242-4E96-9009-30B16BED89AF/Windows6.0-KB968930-x64.msu" "C:\Windows6.0-KB968930-x64.msu" 
+     C:\Windows\System32\cscript.exe a:\downloadFile.vbs "http://download.microsoft.com/download/2/8/6/28686477-3242-4E96-9009-30B16BED89AF/Windows6.0-KB968930-x64.msu" "C:\Windows6.0-KB968930-x64.msu"
      timeout 10 > null
      ECHO "Installing Powershell 2.0"
      C:\Windows\System32\wusa.exe C:\Windows6.0-KB968930-x64.msu /quiet /norestart
@@ -68,6 +68,40 @@ IF "%1%"=="win2008_64" (
      timeout 20 > null
      shutdown /r /t 00
    )
+) ELSE IF "%1%"=="win2003" (
+
+  IF NOT EXIST "C:\WindowsServer2003-KB914961-SP2-x86-ENU.exe" (
+    ECHO "Downloading SP2"
+    C:\Windows\System32\cscript.exe //X a:\downloadFile.vbs "http://download.microsoft.com/download/5/f/1/5f104409-2736-48ef-82e1-692ec3da020b/WindowsServer2003-KB914961-SP2-x86-ENU.exe" "C:\WindowsServer2003-KB914961-SP2-x86-ENU.exe"
+
+    C:\Windows\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v !Setup /t REG_SZ /d "C:\Windows\System32\cmd.exe /c a:\ps.bat win2003" /f
+
+    ECHO "Installing SP2"
+    C:\WindowsServer2003-KB914961-SP2-x86-ENU.exe /quiet /norestart
+  )
+
+  IF NOT EXIST "C:\NetFx20SP1_x86.exe" (
+    ECHO "Downloading .NET 2.0"
+    C:\Windows\System32\cscript.exe //X a:\downloadFile.vbs "http://download.microsoft.com/download/0/8/c/08c19fa4-4c4f-4ffb-9d6c-150906578c9e/NetFx20SP1_x86.exe" "C:\NetFx20SP1_x86.exe"
+
+    ECHO "Installing .NET 2.0"
+    C:\NetFx20SP1_x86.exe /q
+  )
+
+  IF NOT EXIST "WindowsServer2003-KB968930-x86-ENG.exe" (
+
+    ECHO "Downloading Powershell 2.0"
+    C:\Windows\System32\cscript.exe //X a:\downloadFile.vbs "http://download.microsoft.com/download/1/1/7/117FB25C-BB2D-41E1-B01E-0FEB0BC72C30/WindowsServer2003-KB968930-x86-ENG.exe" "WindowsServer2003-KB968930-x86-ENG.exe"
+
+    ECHO "Installing Powershell 2.0"
+    C:\WindowsServer2003-KB968930-x86-ENG.exe /quiet /norestart
+
+    a:\regedit.bat win2003_64
+
+    timeout 20 > null
+    shutdown /r /t 00
+  )
+
 ) ELSE (
   REM unsupported
 )
