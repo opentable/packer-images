66542a24842fa3ac46d13e5915ac805ebf592f68
liamjbennett <lbennett@opentable.com>
2015-09-08 12:28:02 +0100

    fixing the vmware build

--- windows/scripts/winrm.bat
+++ windows/scripts/winrm.bat
@@ -1,9 +1,10 @@
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm quickconfig -transport:http
 cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
-cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB=""}
+cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/service @{MaxConcurrentOperationsPerUser="5000"}
 cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985"
